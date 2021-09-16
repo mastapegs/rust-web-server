b2c7a8e7ecc647f024cce722041622664267f029
Matthew Pagan <mastapegs01@gmail.com>
2021-09-16 16:31:43 -0400

    move html files into a specific folder

--- src/handle_connection.rs
+++ src/handle_connection.rs
@@ -3,26 +3,26 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 pub fn handle_connection(mut stream: TcpStream) {
-  let mut buffer = [0; 1024];
-  stream.read(&mut buffer).unwrap();
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-  let get = b"GET / HTTP/1.1\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
 
-  let (status_line, filename) = if buffer.starts_with(get) {
-      ("HTTP/1.1 200 OK", "hello.html")
-  } else {
-      ("HTTP/1.1 404 NOT FOUND", "404.html")
-  };
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "public/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+    };
 
-  let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
 
-  let response = format!(
-      "{}\r\nContent-Length: {}\r\n\r\n{}",
-      status_line,
-      contents.len(),
-      contents
-  );
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
-  stream.write(response.as_bytes()).unwrap();
-  stream.flush().unwrap();
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
