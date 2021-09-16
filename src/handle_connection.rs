bac5e58d2359d0ab9f0ffcd7fba8798568e95a33
Matthew Pagan <mastapegs01@gmail.com>
2021-09-16 16:21:05 -0400

    initial commit

---
+++ src/handle_connection.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+pub fn handle_connection(mut stream: TcpStream) {
+  let mut buffer = [0; 1024];
+  stream.read(&mut buffer).unwrap();
+
+  let get = b"GET / HTTP/1.1\r\n";
+
+  let (status_line, filename) = if buffer.starts_with(get) {
+      ("HTTP/1.1 200 OK", "hello.html")
+  } else {
+      ("HTTP/1.1 404 NOT FOUND", "404.html")
+  };
+
+  let contents = fs::read_to_string(filename).unwrap();
+
+  let response = format!(
+      "{}\r\nContent-Length: {}\r\n\r\n{}",
+      status_line,
+      contents.len(),
+      contents
+  );
+
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
