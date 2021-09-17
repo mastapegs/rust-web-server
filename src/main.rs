065e86026d347a2910595188f76d90792920d780
Matthew Pagan <mastapegs01@gmail.com>
2021-09-17 09:37:52 -0400

    some changes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use handle_connection::handle_connection;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    println!("Listening on http://127.0.0.1:7878");
+    
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
